3956e732b13311d07f9ece8f647caef5c251de64
Paul R. Hibbing <paulhibbing@gmail.com>
2018-04-05 03:56:12 -0400

    Revise test for CV function speedup

--- tests/testthat/test-CV_function.R
+++ tests/testthat/test-CV_function.R
@@ -1,6 +1,9 @@
+library(TwoRegression)
+context("Sliding Window CV")
+
 testthat::test_that("Revised CV function gives identical results to previous", {
   utils::data(raw_for_cv, package = "TwoRegression")
   old_CV <- get_cvPER_old(raw_for_cv$ENMO)
-  new_CV <- get_CVPER(raw_for_cv$ENMO)
+  new_CV <- get_cvPER(raw_for_cv$ENMO)
   testthat::expect_equal(old_CV, new_CV)
 })
